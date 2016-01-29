6adfe9c6bcdd49eedd2233d9929cf44095a52afb
Roland Pangu <faruzzy@gmail.com>
2016-01-28 19:57:38 -0700

    Initial Commit

---
+++ src/com/faruzzy/dsaj/chpt3/Reinforcement/R_3_2.java
@@ -0,0 +1,50 @@
+package com.faruzzy.dsaj.chpt3.Reinforcement;
+import com.faruzzy.dsaj.utils.Util;
+import java.util.Random;
+
+/**
+ * Created by faruzzy on 1/28/16.
+ */
+public class R_3_2 {
+    private static int[][] array;
+    private Random random;
+
+    public R_3_2() {
+        array = new int[6][6];
+        random = new Random();
+        generateValues();
+        Util.printMultiDimensionalArray(array);
+    }
+
+    public int addAllArrayElements(int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++)
+            sum += binarySum(array[i], 0, array.length - 1);
+        return sum;
+    }
+
+    private static int binarySum(int[] array, int low, int high) {
+        if (low > high) return 0;
+        if (low == high) return array[low];
+        int mid = (low + high) / 2;
+        return binarySum(array, low, mid) + binarySum(array, mid + 1, high);
+    }
+
+    private void generateValues() {
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                array[i][j] = random.nextInt(100);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        R_3_2 obj = new R_3_2();
+
+        //testing binarySum
+        //int[] arr =  { 1, 2, 3, 4, 5, 6, 7 };
+        //System.out.println(binarySum(arr, 0, arr.length - 1));
+        Util.println("Total: " + obj.addAllArrayElements(array));
+    }
+}
---
+++ src/com/faruzzy/dsaj/utils/Util.java
@@ -0,0 +1,38 @@
+package com.faruzzy.dsaj.utils;
+
+/**
+ * Created by faruzzy on 1/28/16.
+ */
+public class Util {
+
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+
+    public static void println() {
+        System.out.println();
+    }
+
+    public static void println(String s) {
+        System.out.println(s);
+    }
+
+    public static void printArray(int[] array) {
+        String s = "[ ";
+        for (int i = 0; i < array.length; i++) {
+            s += array[i] + " ";
+        }
+        s += "]";
+        println(s);
+    }
+
+    public static void printMultiDimensionalArray(int array[][]) {
+        int len = array.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++)
+                System.out.printf("%d ", array[i][j]);
+            println();
+        }
+    }
+}
