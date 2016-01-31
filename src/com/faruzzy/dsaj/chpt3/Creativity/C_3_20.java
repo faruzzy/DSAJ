cb2c89d594a07a995ff8446f6e6b552ce1757e3a
Roland Pangu <faruzzy@gmail.com>
2016-01-31 00:59:28 -0700

    Added C-3.20 solution

---
+++ src/com/faruzzy/dsaj/chpt3/Creativity/C_3_20.java
@@ -0,0 +1,33 @@
+package com.faruzzy.dsaj.chpt3.Creativity;
+import com.faruzzy.dsaj.utils.Util;
+
+/**
+ * Created by faruzzy on 1/31/16.
+ * C-3.20
+ * Use recursion to write a Java method for determining if a string s has more vowels than consonants.
+ */
+public class C_3_20 {
+    private static Character[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+    public static boolean hasMoreVowelsThanConsonants(String s, int i, int vcounter, int ccounter) {
+        if (i < 0) return vcounter > ccounter;
+        if (Util.indexOf(vowels, s.charAt(i)) != -1) {
+            ++vcounter;
+        } else {
+           ++ccounter;
+        }
+        return hasMoreVowelsThanConsonants(s, --i, vcounter, ccounter);
+    }
+
+    public static void main(String[] args) {
+        //String str = "history";
+        String str = "auopmousie";
+        boolean result = hasMoreVowelsThanConsonants(str, str.length() - 1, 0, 0);
+        if (result) {
+            Util.println(str + " has more vowels than consonants");
+        } else {
+            Util.println(str + " has more consonants than vowels");
+        }
+    }
+
+}
