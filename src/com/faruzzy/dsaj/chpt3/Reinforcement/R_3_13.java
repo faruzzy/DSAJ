1f974c66ee829099981f685d1207cb0ef454fcab
Roland Pangu <faruzzy@gmail.com>
2016-01-30 17:47:05 -0700

    Added 3.13 & 3.14

---
+++ src/com/faruzzy/dsaj/chpt3/Reinforcement/R_3_13.java
@@ -0,0 +1,32 @@
+package com.faruzzy.dsaj.chpt3.Reinforcement;
+import com.faruzzy.dsaj.utils.Util;
+import java.util.Random;
+
+/**
+ * Created by faruzzy on 1/30/16.
+ */
+public class R_3_13 {
+
+    public static void clearArray(int[] array) {
+        Random random = new Random();
+        while(!isEmpty(array)) {
+            int index = random.nextInt(array.length);
+            if (array[index] != -1)
+                array[index] = -1;
+        }
+    }
+
+    private static boolean isEmpty(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            if (array[i] != -1) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        array = Util.generateArrayValues(array, 40);
+        Util.printArray(array);
+        clearArray(array);
+        Util.printArray(array);
+    }
+}
---
+++ src/com/faruzzy/dsaj/chpt3/Reinforcement/R_3_14.java
@@ -0,0 +1,30 @@
+package com.faruzzy.dsaj.chpt3.Reinforcement;
+
+import com.faruzzy.dsaj.chpt3.DNodeString;
+import com.faruzzy.dsaj.chpt3.DLinkedListString;
+import com.faruzzy.dsaj.utils.Util;
+
+/**
+ * Created by faruzzy on 1/30/16.
+ */
+public class R_3_14 {
+    private static int count(DLinkedListString list) {
+        int count = 0;
+        DNodeString current = list.header.getNext();
+        while (current != list.trailer) {
+            count++;
+            current = current.getNext();
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        DLinkedListString list = new DLinkedListString();
+        list.addFirst("Roland");
+        list.addFirst("Patrick");
+        list.addFirst("Junior");
+        list.addFirst("Rachel");
+        list.addLast("Amundala");
+        Util.println("Count: " + count(list));
+    }
+}
