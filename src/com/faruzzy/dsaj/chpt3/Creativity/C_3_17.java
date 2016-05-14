1dde1bcb90821fe8fc52a544d24e0b4d8cc52169
Roland Pangu <faruzzy@gmail.com>
2016-05-14 11:15:43 -0700

    Simplify a little

--- src/com/faruzzy/dsaj/chpt3/Creativity/C_3_17.java
+++ src/com/faruzzy/dsaj/chpt3/Creativity/C_3_17.java
@@ -7,32 +7,27 @@ import com.faruzzy.dsaj.utils.u;
  * of int values so that all the even values appear before all the odd values.
  */
 public class C_3_17 {
-    public static void arrangeEvenBeforeOdd(int[] array, int start, int end) {
-        boolean allOdds = false;
-        int e = end - 1;
-        if (start < end) {
+    public static void arrangeEvenBeforeOdd(int[] array, int start) {
+        int len = array.length;
+        if (start < len) {
             while(array[start] % 2 != 0) {
-                if (e == start) {
-                    allOdds = true;
-                    break;
-                }
+                if (len == start) return;
 
-                if (array[e] % 2 == 0) {
+                if (array[len - 1] % 2 == 0) {
                     int temp = array[start];
-                    array[start] = array[e];
-                    array[e] = temp;
+                    array[start] = array[len - 1];
+                    array[len - 1] = temp;
                 } else {
-                    e--;
+                    len--;
                 }
             }
-            if (allOdds) return;
-            arrangeEvenBeforeOdd(array, ++start, --end);
+            arrangeEvenBeforeOdd(array, ++start);
         }
     }
 
     public static void main(String[] args) {
         int[] array = { 9, 7, 5, 3, 2, 4, 6, 8, 10 };
-        arrangeEvenBeforeOdd(array, 0, array.length);
+        arrangeEvenBeforeOdd(array, 0);
         u.printArray(array);
     }
 }
